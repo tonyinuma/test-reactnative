2333ff4c4377a232c56e85e1d720fe1731d127ee
Tony ID <tony.inuma@gmail.com>
2021-04-06 10:42:10 -0500

    Feat: Get API and Render in card, usign button

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Card, Button, Icon} from 'react-native-elements'
+
+const dataURL = 'https://0q27loouph.execute-api.us-east-1.amazonaws.com';
 
 const App = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    const loadData = () => {
+        fetch(dataURL)
+            .then((res) => res.json())
+            .then(data => setData(data))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false))
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>lets work bro! ♥</Text>
-            <StatusBar style="auto"/>
+            <Card>
+                <Card.Title>
+                    {data.name}
+                    {"\n"}
+                    {data.nutritionist}
+                </Card.Title>
+                <Card.Divider/>
+                <Card.Image source={{uri: data.image}}>
+                </Card.Image>
+
+                <Text style={{marginBottom: 10}}>
+                    <Icon name='email' color='blue'/>{data.email}
+                </Text>
+                <Text style={{marginBottom: 10}}>
+                    <Icon name='phone' color='blue'/>
+                    {data.phone}
+                </Text>
+                <Button
+                    onPress={() => {
+                        loadData()
+                    }}
+                    icon={<Icon name='repeat' color='#ffffff'/>}
+                    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                    title='Cambiar'/>
+            </Card>
         </View>
     );
 }
@@ -14,7 +56,7 @@ const App = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000000',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
